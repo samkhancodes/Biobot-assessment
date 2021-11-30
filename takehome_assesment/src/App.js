b0cdb4d0eb8c093d3b33be1c25309856fa5a340c
samkhancodes <93014752+samkhancodes@users.noreply.github.com>
2021-11-30 16:29:30 -0500

    feat: Implement Search Kit
    Create a React Project and implement search kit using React bootsrap and items can be search by label_ID.

---
+++ takehome_assesment/src/App.js
@@ -0,0 +1,13 @@
+import react from "react";
+import TableList from "./Components/TableList";
+import SearchPage from "./Components/SearchPage";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchPage />
+    </div>
+  );
+}
+
+export default App;
